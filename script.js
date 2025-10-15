dcdc1bdf9550d8698c36872f0e2e7d9216b49e1e
Ford Berry <fordaberry@gmail.com>
2025-10-14 21:57:45 -0600

    Get random character, generate password, and display the password to the page

--- script.js
+++ script.js
@@ -4,6 +4,41 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
     "$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
+let passwordLength = 15
+// select password boxes
+let passwordBox1 = document.getElementById("password-box-1")
+let passwordBox2 = document.getElementById("password-box-2")
+// select password button
+let passwordButton = document.getElementById("generate-passwords-btn")
+
+function getRandomCharacter() {
+    // select random character from array
+    let randomChar = Math.floor(Math.random() * characters.length)
+    return characters[randomChar]
+}
+
+function generateRandomPassword() {
+    let randomPassword = ""
+    // loop through array
+    for (let i = 0; i < passwordLength; i++) {
+        randomPassword += getRandomCharacter()
+    }
+    return randomPassword
+}
+
+const generatedPasswordOne = generateRandomPassword()
+const generatedPasswordTwo = generateRandomPassword()
+
+// test button click
+passwordButton.addEventListener("click", function() {
+    passwordBox1.textContent = generatedPasswordOne
+    passwordBox2.textContent = generatedPasswordTwo
+})
+
+
+
+
+
 
 
 
